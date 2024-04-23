002f42302186f816560f07ef555fde483c620fa1
Sat? <140927620+JezzXyz@users.noreply.github.com>
2024-04-23 15:42:11 +0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -15,43 +15,40 @@ global.bot = {
     name: 'Rei Ayanami',
     prefix: /^[°•π÷×¶∆£¢€¥®™+✓_=|/~!?@#%^&.©^]/i,
     thumbnail: 'https://camo.githubusercontent.com/a71a3d7b6e8cee0a85fe87e32cfd860a034603b97bbb8fb98d2a12828d6a3005/68747470733a2f2f696d61676573332e616c706861636f646572732e636f6d2f3636322f7468756d622d3335302d3636323231392e706e67',
-    groupChat: 'https://chat.whatsapp.com/FlqTGm4chSjKMsijcqAIJs'
+    groupChat: ''
 };
 
 // MSG (Message / Pesan)
 global.msg = {
     // Akses perintah
     admin: `${bold('[ ! ]')} Perintah hanya dapat diakses oleh admin grup!`,
-    botAdmin: `${bold('[ ! ]')} Bot bukan admin, tidak bisa menggunakan perintah!`,
     owner: `${bold('[ ! ]')} Perintah hanya dapat diakses Owner!`,
     group: `${bold('[ ! ]')} Perintah hanya dapat diakses dalam grup!`,
     private: `${bold('[ ! ]')} Perintah hanya dapat diakses dalam obrolan pribadi!`,
 
     // Antarmuka perintah
     watermark: `${package.name}@^${package.version}`,
-    footer: quote('Dibuat oleh ItsReimau | Take care of yourself.'),
-    readmore: '\u200E'.repeat(4001),
+    footer: quote('Dibuat oleh @jisaat_ | Take care of yourself.'),
 
     // Proses perintah
     argument: `${bold('[ ! ]')} Masukkan argumen!`,
-    wait: `${bold('[ ! ]')} Tunggu sebentar...`,
-
-    // Proses perintah (Kesalahan)
-    notFound: `Tidak ada yang ditemukan!`,
-    urlInvalid: `URL tidak valid!`
+    botAdmin: `${bold('[ ! ]')} Bot bukan admin, tidak bisa menggunakan perintah!`,
+    notFound: `${bold('[ ! ]')} Tidak ada hasil yang ditemukan!`,
+    urlInvalid: `${bold('[ ! ]')} URL tidak valid!`,
+    wait: `${bold('[ ! ]')} Tunggu sebentar...`
 };
 
 // Owner
 global.owner = {
     name: 'ItsReimau',
-    number: '6283838039693',
+    number: '6281226769432',
     organization: 'Kumaha Aing'
 };
 
 // Sticker
 global.sticker = {
     packname: 'Take care of yourself.',
-    author: '@rei-ayanami'
+    author: '@jisaat_'
 };
 
 // System
